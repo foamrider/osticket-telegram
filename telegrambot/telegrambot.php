59048c870e6a6027a003218b72a0ff688ad98198
Tomas Kirkegaard <tomas.kirkegaard@gmail.com>
2017-12-06 11:54:54 +0100

    Fix #4

--- telegrambot/telegrambot.php
+++ telegrambot/telegrambot.php
@@ -19,26 +19,26 @@ class TelegramPlugin extends Plugin {
         $title = $ticket->getSubject() ?: 'No subject';
 		$createdBy = $ticket->getName()." (".$ticket->getEmail().")";
 		$chatid = $this->getConfig()->get('telegram-chat-id');
-		
+
 		$this->sendToTelegram(
 			array(
 				"method" => "sendMessage",
 				"chat_id" => $chatid,
-				"text" => "*New Ticket:* [#".$ticketId."](".$ticketLink.")\n*Subject:* ".$title."\n*Created by:* ".$createdBy,
-				"parse_mode" => "Markdown",
+				"text" => "<b>New Ticket:</b> <a href=\"".$ticketLink."\">#".$ticketId."</a>\n<b>Subject:</b> ".$title."\n<b>Created by:</b> ".$createdBy,
+				"parse_mode" => "html",
 				"disable_web_page_preview" => "True"
 			)
 		);
 	}
-	
+
 	function sendToTelegram($payload)
     {
         try {
             global $ost;
-     
+
             $data_string = utf8_encode(json_encode($payload));
             $url = $this->getConfig()->get('telegram-webhook-url');
-     
+
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
@@ -48,29 +48,29 @@ class TelegramPlugin extends Plugin {
                     'Content-Length: ' . strlen($data_string)
                 )
             );
-     
+
             if (curl_exec($ch) === false) {
                 throw new Exception($url . ' - ' . curl_error($ch));
             } else {
                 $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-     
+
                 if ($statusCode != '200') {
                     throw new Exception($url . ' Http code: ' . $statusCode);
                 }
             }
- 
+
             curl_close($ch);
         } catch(Exception $e) {
             error_log('Error posting to Telegram. '. $e->getMessage());
         }
     }
- 
+
     function escapeText($text)
     {
         $text = str_replace('&', '&amp;', $text);
         $text = str_replace('<', '&lt;', $text);
         $text = str_replace('>', '&gt;', $text);
- 
+
         return $text;
     }
 }
